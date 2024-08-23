06bf91b1025dbf93c114cf250c3613bdea78e4d1
YURI SILVA PLOTZE <yuri_plotze@estudante.sesisenai.org.br>
2024-08-22 21:37:18 -0300

    novo

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,28 @@
-
+import './styles.css'
 
 export function Home() {
 
   return (
-    <h1>
-      Pagina principal
-    </h1>
+    <>
+      <img src="../../../public/img/image.png" alt=""/>
+
+      <h1><span class="sombra">Transformando </span><span class="coloridoazul">espaço</span>, <br/> criando <span class="coloridoazul">história</span> </h1>
+    <p1>Nós criamos ambientes  <b> exclusívos </b> e <b> únicos </b>, com muito bom gosto e profissionalismo, <br/> investimos tempo e 
+        <span class="coloridoazul"> dedicação no seu projeto</span>. Nosso compromisso vai além de simplesmente decorar.</p1>
+        <br/>
+        <br/>
+    <p2>Tenha <span class="coloridoazul">ambientes limpos</span> e bem decorados, transmitindo <b>elegância</b> e <b> finesse </b>para todo <br/> aquele que estiver ali. Sua história visual começa aqui!</p2>
+
+    <footer>
+
+      <div class="linha" ></div>
+
+      <a hre f="instagram.com" class="coloridoazul" id="insta">Instagram</a>
+      &nbsp;&nbsp;&nbsp;&nbsp;
+
+    <a href="whatsapp.com" class="coloridoazul">Fale Conosco</a>
+
+    </footer>
+    </>
   )
 }
